efb8bb50a55b74d255d66df8c00c7744d80c62ef
junseokoh-hub <lunulatecat@naver.com>
2022-03-19 19:41:28 +0900

    add-todo-list

---
+++ js/todo-list.js
@@ -0,0 +1,71 @@
+const toDoForm = document.querySelector(".todo-form");
+const toDoInput = document.querySelector(".todo-input");
+const toDoAddButton = document.querySelector(".todo-button__add");
+const toDoList = document.querySelector(".todo-list");
+
+const TODOS = "todos"
+
+let toDos = []; 
+
+
+function addToDo(event) {
+    event.preventDefault();
+    const line = toDoInput.value;
+    toDoInput.value = "";
+    const lineObject = {
+        text: line,
+        id: Date.now(),
+    }
+    paintToDo(lineObject);
+    toDos.push(lineObject);
+    saveToDo();
+}
+
+function paintToDo (line) {
+    //add li
+    const myToDo = document.createElement("li");
+    myToDo.classList.add("todo-mine");
+    myToDo.id = line.id;
+    myToDo.innerText = line.text;
+    toDoList.appendChild(myToDo);
+    //add check btn
+    const checkButton = document.createElement("button");
+    checkButton.classList.add("todo-button__check");
+    checkButton.innerHTML = "<i class='fas fa-check'></i>"
+    checkButton.addEventListener("click", removeToDo);
+    myToDo.appendChild(checkButton);
+    //add remove btn
+    const removeButton = document.createElement("button");
+    removeButton.classList.add("todo-button__remove");
+    removeButton.innerHTML = "<i class='fas fa-trash'></i>"
+    removeButton.addEventListener("click", removeToDo);
+    myToDo.appendChild(removeButton);
+}
+
+function removeToDo(event) {
+    const btn = event.target;
+    const li = event.target.parentElement;
+    if (btn.classList[0] === "todo-button__remove") {
+        li.remove();
+    }
+    if(btn.classList[0] === "todo-button__check") {
+        li.classList.toggle("checked");
+    }
+    toDos = toDos.filter((delId) => delId.id !== parseInt(li.id));
+    saveToDo();
+}
+
+
+function saveToDo() {
+    localStorage.setItem(TODOS, JSON.stringify(toDos));
+}
+
+const savedToDos = localStorage.getItem(TODOS);
+
+if (savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
+
+toDoAddButton.addEventListener("click", addToDo);
